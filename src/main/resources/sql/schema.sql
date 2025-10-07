2950fe4db23a2400a2f85ea22f7c2885ff210d03
Frede-72 <frjo0005@stud.kea.dk>
2025-10-07 11:14:01 +0200

    updated sql scripts

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,33 +1,38 @@
 drop
-database if exists touristGuide;
+database if exists tourist_Guide;
 CREATE
-DATABASE IF NOT EXISTS touristGuide
+DATABASE IF NOT EXISTS tourist_Guide
   CHARACTER SET utf8mb4;
 
 USE
-touristGuide;
+tourist_Guide;
 
 create table tourist_attractions
 (
-    name                varchar(255)  not null,
-    description         varchar(1000) not null,
-    city                varchar(255)  not null,
-    ticket_price_in_DKK int           not null,
+    name        varchar(255)  not null,
+    description varchar(1000) not null,
+    city        varchar(255)  not null,
+    ticket_price_in_DKK double not null,
     primary key (name)
 );
 
 create table tag
 (
-    id   int          not null auto_increment,
     name varchar(155) not null,
-    primary key (id, name)
+    primary key (name)
+);
+
+create table city
+(
+    name varchar(155) not null,
+    primary key (name)
 );
 
 create table tags
 (
     attraction_name varchar(255) not null,
-    tag_id          int          not null,
-    primary key (attraction_name, tag_id),
+    tag_name        varchar(155) not null,
+    primary key (attraction_name, tag_name),
     foreign key (attraction_name) references tourist_attractions (name)
         on delete cascade
 );
